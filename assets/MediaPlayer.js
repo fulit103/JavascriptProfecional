ec55e38869636dd11e795d1b94e8b34f8aa61223
Julian Toro <jtoro@tecmovin.com>
2019-09-19 18:09:24 -0500

    ejercicios

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,21 +1,46 @@
 function MediaPlayer(config) {
-    this.media = config.el;
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+  
+MediaPlayer.prototype._initPlugins = function (){
+  this.plugins.forEach( plugin=>{
+    plugin.run(this);
+  })
+}
+
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function(){
+  this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function(){
+  this.media.muted = false;
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
   }
-  
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
-  
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
-  
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
-  
-  export default MediaPlayer;
+};
+
+MediaPlayer.prototype.toggleMute = function() {
+  if (this.media.muted) {
+    this.unmute();
+  } else {
+    this.mute();
+  }
+};
+
+export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,12 @@
-import MediaPlayer from './MediaPlayer.js.js';
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
 
+
+const plugins = [ new AutoPlay() ]
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video , plugins:plugins});
 
-const button = document.querySelector('button');
+const button = document.querySelector('.play');
 button.onclick = () => player.togglePlay();
+const muteToggle = document.querySelector(".toggle-mute");
+muteToggle.onclick = () => player.toggleMute();
